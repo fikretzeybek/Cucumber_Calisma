d0240583126a82cf1db81280a84a541dfcca1862
fikretzeybek <fzeybek1972@gmail.com>
2023-12-21 21:59:24 +0300

    ek

--- src/test/java/stepdefinitions/WebdriverUniversityStepdefinitions.java
+++ src/test/java/stepdefinitions/WebdriverUniversityStepdefinitions.java
@@ -1,6 +1,48 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
 public class WebdriverUniversityStepdefinitions {
 
+    @Given("ourProduct linkine tiklamak icin iFrame'e gecis yapar")
+    public void our_product_linkine_tiklamak_icin_i_frame_e_gecis_yapar() {
+
+    }
+    @Given("Our Products butonuna basar")
+    public void our_products_butonuna_basar() {
+        // Wite code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("acilan sayfada Cameras'i tiklar")
+    public void acilan_sayfada_cameras_i_tiklar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Popup mesajini yazdirir")
+    public void popup_mesajini_yazdirir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("close butonuna basar")
+    public void close_butonuna_basar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("iFrame'den cikis yapar")
+    public void i_frame_den_cikis_yapar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("WebdriverUniversity.com \\(IFrame) linkini tiklar")
+    public void webdriver_university_com_i_frame_linkini_tiklar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("acilan sayfa url'inin {string} oldugunu test eder")
+    public void acilan_sayfa_url_inin_oldugunu_test_eder(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
