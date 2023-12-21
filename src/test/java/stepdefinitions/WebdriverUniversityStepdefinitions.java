5250e3af5897cffbc180ac0ca6976fae29ff3b43
fikretzeybek <fzeybek1972@gmail.com>
2023-12-21 22:17:57 +0300

    ek4

--- src/test/java/stepdefinitions/WebdriverUniversityStepdefinitions.java
+++ src/test/java/stepdefinitions/WebdriverUniversityStepdefinitions.java
@@ -12,30 +12,37 @@ public class WebdriverUniversityStepdefinitions {
     public void our_product_linkine_tiklamak_icin_i_frame_e_gecis_yapar() {
         Driver.getDriver().switchTo().frame(page.iFrameElementi);
     }
+
     @Given("Our Products butonuna basar")
     public void our_products_butonuna_basar() {
         page.ourProductLinki.click();
     }
+
     @Then("acilan sayfada Cameras'i tiklar")
     public void acilan_sayfada_cameras_i_tiklar() {
-
+        page.camerasElementi.click();
     }
+
     @Then("Popup mesajini yazdirir")
     public void popup_mesajini_yazdirir() {
-
+        System.out.println("Alert yazi : " + page.alertYaziElementi);
     }
+
     @Then("close butonuna basar")
     public void close_butonuna_basar() {
-
+        page.alertKapatButonu.click();
     }
+
     @Then("iFrame'den cikis yapar")
     public void i_frame_den_cikis_yapar() {
 
     }
+
     @Then("WebdriverUniversity.com \\(IFrame) linkini tiklar")
     public void webdriver_university_com_i_frame_linkini_tiklar() {
 
     }
+
     @Then("acilan sayfa url'inin {string} oldugunu test eder")
     public void acilan_sayfa_url_inin_oldugunu_test_eder(String string) {
 
