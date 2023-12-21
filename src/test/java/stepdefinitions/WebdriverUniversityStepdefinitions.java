8e480a0f140a4411bc1a7e7e140227e43618f7e5
fikretzeybek <fzeybek1972@gmail.com>
2023-12-21 22:00:44 +0300

    ek2

--- src/test/java/stepdefinitions/WebdriverUniversityStepdefinitions.java
+++ src/test/java/stepdefinitions/WebdriverUniversityStepdefinitions.java
@@ -11,38 +11,31 @@ public class WebdriverUniversityStepdefinitions {
     }
     @Given("Our Products butonuna basar")
     public void our_products_butonuna_basar() {
-        // Wite code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("acilan sayfada Cameras'i tiklar")
     public void acilan_sayfada_cameras_i_tiklar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("Popup mesajini yazdirir")
     public void popup_mesajini_yazdirir() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("close butonuna basar")
     public void close_butonuna_basar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("iFrame'den cikis yapar")
     public void i_frame_den_cikis_yapar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("WebdriverUniversity.com \\(IFrame) linkini tiklar")
     public void webdriver_university_com_i_frame_linkini_tiklar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("acilan sayfa url'inin {string} oldugunu test eder")
     public void acilan_sayfa_url_inin_oldugunu_test_eder(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 }
