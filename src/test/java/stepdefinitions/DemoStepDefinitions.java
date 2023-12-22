76ff678be73d61cc0975571c4e022eb2fb3df666
fikretzeybek <fzeybek1972@gmail.com>
2023-12-22 23:26:50 +0300

    ek5

--- src/test/java/stepdefinitions/DemoStepDefinitions.java
+++ src/test/java/stepdefinitions/DemoStepDefinitions.java
@@ -11,8 +11,6 @@ public class DemoStepDefinitions {
     @Given("kullanici satir nosu {string} ve sutun nosu {string} olan hucredeki yaziyi yazdirir")
     public void kullanici_satir_nosu_ve_sutun_nosu_olan_hucredeki_yaziyi_yazdirir(String satirNo, String sutunNo) {
 
-        int satirNumarasi= Integer.parseInt(ConfigReader.getProperty(satirNo));
-        int sutunNumarasi=Integer.parseInt(ConfigReader.getProperty(sutunNo));
 
 
         System.out.println("istediginiz hucredeki yazi : " + demoqaPage.istenenHucreBilgisiniDondur(satirNumarasi,sutunNumarasi));
